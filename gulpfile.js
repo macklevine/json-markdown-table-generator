92c7e1de37e5aa0528c93e85779577b7e0c4177d
macklevine <mackplevine@gmail.com>
2016-09-17 18:12:56 -0700

    made changes to gulpfile, including javascript concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,54 @@ var gulp = require('gulp');
 var less = require('less');
 var fs = require('fs');
 var server = require('gulp-develop-server');
+var minify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 //set up the watchers only once.
-gulp.task('default', ['compile-less', 'start-server', 'watch-for-changes']);
+gulp.task('default', ['compile-less', 'build-javascript-dev', 'scoot-templates', 'start-server', 'watch-for-changes']);
 
 gulp.task('compile-less', function(){
 	fs.readFile('./client/less/main.less', 'utf-8', function (e, contents){
 		less.render(contents, function (e, output){
-			fs.writeFile('./client/less/main.css', output.css);
-			fs.writeFile('./client/js/main.css', output.css);
+			fs.writeFile('./client/build/main.css', output.css);
 		});
 	});
 });
 
-gulp.task( 'start-server', function() {
+var javaScriptSources = [
+	'./bower_components/angular/angular.js',
+	'./bower_components/jquery/jquery.js',
+	'./client/js/**/*.js'
+];
+
+
+gulp.task('build-javascript-dev', function() {
+  return gulp.src(javaScriptSources)
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./client/build/js'));
+});
+
+gulp.task('scoot-templates', function () {
+	return gulp.src([
+		'./client/html/*.html'
+	])
+	.pipe(gulp.dest('./client/build/'));
+});
+
+
+gulp.task('start-server', function() {
     server.listen( { path: './server/server.js' } );
 });
 
+gulp.task('buildJavaScriptRelease', function () {
+
+});
+
 gulp.task('watch-for-changes', function(){
-	gulp.watch(['./client/js/*.js','./client/js/*.html','./server/*'], server.restart);
-	gulp.watch('./client/less/*', ['compile-less', server.restart]);
+	gulp.watch(['./client/js/**/*.js'], ['build-javascript-dev']);
+	gulp.watch(['./client/html/**/*.html'], ['scoot-templates']);
+	gulp.watch(['./server/**/*.js'], ['restart-server']);
+	gulp.watch(['./client/less/*'], ['compile-less', 'restart-server']);
 });
 
  
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 console.log(__dirname);
 
-app.use(express.static(__dirname + "/../client/js/"));
+app.use(express.static(__dirname + "/../client/build/"));
 //configure routes
 router.configureRoutes(app);
 
